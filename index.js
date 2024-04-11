1b5135a31dc37d5f1367ed87cfd31290580753b1
Aaron2963 <neotgrr@gmail.com>
2024-04-11 18:31:00 +0800

    加入用戶驗證及記錄

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { readFileSync, readdirSync, writeFileSync } from "fs";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import express from "express";
+import winston from "winston";
 import Client from "./model/client.js";
 import Credential from "./credential.js";
 import Auth from "./auth.js";
@@ -22,8 +23,31 @@ const credentials = {};
 const apps = {};
 let auth;
 
+const logger = winston.createLogger({
+  // Log only if level is less than (meaning more severe) or equal to this
+  level: "info",
+  // Use timestamp and printf to create a standard log format
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(
+      (info) => `${info.timestamp} ${info.level}: ${info.message}`
+    )
+  ),
+  // Log to the console and a file
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/app.log" }),
+  ],
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  //record user ip
+  const ip = req.socket.remoteAddress;
+  logger.info(`Received a ${req.method} request for ${req.url} from ${ip}`);
+  next();
+});
 
 function init() {
   let config = readFileSync(resolve(__dirname, "config.json"), "utf-8");
@@ -72,6 +96,33 @@ function initializaApp(req, res, next) {
   next();
 }
 
+function authorize(req, res, next) {
+  if (auth == null) {
+    res.status(500).send("Auth not initialized");
+    return;
+  }
+  try {
+    const token = req.headers.authorization,
+      projectId = req.headers["x-project-id"];
+    if (!token) {
+      throw new Error("Bearer token not found");
+    }
+    const parts = token.split(" ");
+    if (parts.length !== 2) {
+      throw new Error("Invalid token format");
+    }
+    const decoded = auth.authenticate(parts[1], projectId);
+    if (!decoded) {
+      throw new Error("Invalid token");
+    }
+    next();
+  } catch (error) {
+    console.log("authorize failed:", error);
+    res.status(401).send("Unauthorized");
+    return;
+  }
+}
+
 app.post("/token", (req, res) => {
   const { client_id, client_secret, grant_type } = req.body;
   if (!client_id || !client_secret || !grant_type) {
@@ -94,7 +145,7 @@ app.post("/token", (req, res) => {
   }
 });
 
-app.post("/credentials", (req, res) => {
+app.post("/credentials", [authorize], (req, res) => {
   const { key } = req.body;
   const projectId = req.headers["x-project-id"];
   if (!projectId || !projects.includes(projectId)) {
@@ -113,19 +164,19 @@ app.post("/credentials", (req, res) => {
   }
 });
 
-app.get("/auth/user", initializaApp, async (req, res) => {
+app.get("/auth/user", [authorize, initializaApp], async (req, res) => {
   const users = await new Auth(apps[req.query.projectId]).listUsers();
   res.json(users);
 });
 
-app.get("/auth/user/:uid", initializaApp, async (req, res) => {
+app.get("/auth/user/:uid", [authorize, initializaApp], async (req, res) => {
   const user = await new Auth(apps[req.query.projectId]).getUser(
     req.params.uid
   );
   res.json(user);
 });
 
-app.post("/auth/user", initializaApp, async (req, res) => {
+app.post("/auth/user", [authorize, initializaApp], async (req, res) => {
   const { email, uid } = req.body;
   const userId = await new Auth(apps[req.query.projectId]).createUser(
     email,
@@ -134,31 +185,39 @@ app.post("/auth/user", initializaApp, async (req, res) => {
   res.json({ userId });
 });
 
-app.delete("/auth/user/:uid", initializaApp, async (req, res) => {
+app.delete("/auth/user/:uid", [authorize, initializaApp], async (req, res) => {
   await new Auth(apps[req.query.projectId]).deleteUser(req.params.uid);
   res.status(204).send();
 });
 
-app.get("/auth/custom-token/:uid", initializaApp, async (req, res) => {
-  const token = await new Auth(apps[req.query.projectId]).createCustomToken(
-    req.params.uid
-  );
-  res.json({ token });
-});
-
-app.post("/auth/id-token/verify", initializaApp, async (req, res) => {
-  const { idToken } = req.body;
-  try {
-    const token = await new Auth(apps[req.query.projectId]).verifyIdToken(
-      idToken
+app.get(
+  "/auth/custom-token/:uid",
+  [authorize, initializaApp],
+  async (req, res) => {
+    const token = await new Auth(apps[req.query.projectId]).createCustomToken(
+      req.params.uid
     );
-    res.json(token);
-  } catch (error) {
-    res.status(401).send(error.message);
+    res.json({ token });
   }
-});
+);
 
-app.get("/fs/doc", initializaApp, async (req, res) => {
+app.post(
+  "/auth/id-token/verify",
+  [authorize, initializaApp],
+  async (req, res) => {
+    const { idToken } = req.body;
+    try {
+      const token = await new Auth(apps[req.query.projectId]).verifyIdToken(
+        idToken
+      );
+      res.json(token);
+    } catch (error) {
+      res.status(401).send(error.message);
+    }
+  }
+);
+
+app.get("/fs/doc", [authorize, initializaApp], async (req, res) => {
   try {
     const doc = await new Document(apps[req.query.projectId]).getDoc(
       req.query.path
@@ -170,7 +229,7 @@ app.get("/fs/doc", initializaApp, async (req, res) => {
   }
 });
 
-app.post("/fs/doc", initializaApp, async (req, res) => {
+app.post("/fs/doc", [authorize, initializaApp], async (req, res) => {
   try {
     await new Document(apps[req.query.projectId]).setDocument(
       req.query.path,
